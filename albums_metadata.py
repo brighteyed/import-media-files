94c2886dbc7e65406fa28ed8b42ba2ebb81caf7f
kondrikov <kondrikov@ascon.ru>
2019-01-11 11:33:49 +0300

    Refactor albums_metadata.py

--- albums_metadata.py
+++ albums_metadata.py
@@ -8,17 +8,64 @@ import os
 import re
 import shutil
 import subprocess
-import sys
 import uuid
 import zipfile
-
 import exifread
+
+def process_photo(photo_file):
+    with open(photo_file, 'rb') as file:
+        try:
+            tags = exifread.process_file(file, details=False)
+            if 'EXIF DateTimeOriginal' not in tags.keys():
+                print("[ERROR] Missing EXIF info. Can't place photo {0} into album {1}".format(photo_file, albumdata['title']))
+                return ''
+
+            dt = datetime.datetime.strptime(str(tags['EXIF DateTimeOriginal']).split(' ')[0], '%Y:%m:%d')
+            return os.sep.join([dt.strftime('%Y-%m-%d'), os.path.basename(photo_file)])
+
+        except PermissionError:
+            print("[ERROR] PermissionError. Can't place photo {0} into album {1}".format(photo_file, albumdata['title']))
+
+    return ''
+
+def process_video(video_file):
+    cmnd = ['ffprobe.exe', '-show_streams', '-print_format', 'json', '-loglevel', 'quiet', '{0}'.format(video_file)]
+    p = subprocess.Popen(cmnd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    out, _err = p.communicate()
+    
+    info = json.loads(out.decode('utf-8'))
+    creation_time_found = False
+
+    for stream in info['streams']:
+        if not 'tags' in stream:
+            continue
+
+        tags = stream['tags']
+
+        if 'creation_time' in tags:
+            creation_time_found = True
+
+            dt = datetime.datetime.strptime(tags['creation_time'], '%Y-%m-%dT%H:%M:%S.%fZ')
+            return os.sep.join([dt.strftime('%Y-%m-%d'), os.path.basename(video_file)])
+
+    if not creation_time_found:
+        print('[ERROR] Creation time not found in {0}'.format(video_file))
+
+    return ''
+
+
+def safe_remove_file(file):
+    """ Remove file without exceptions thrown """
+    try:
+        os.remove(tmp_file)
+    except OSError:
+        pass
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Create albums description')
+    parser = argparse.ArgumentParser(description='Create metadata files describing albums')
     parser.add_argument('--takeout-dir', type=str,
-                        help='Directory with files')
+                        help='Google Photo takeout directory')
     parser.add_argument('--out-dir', type=str,
                         help='Destination directory')
 
@@ -29,20 +76,20 @@ if __name__ == '__main__':
     tmp_dir = os.path.join(out_dir, '.tmp')
     os.makedirs(tmp_dir, exist_ok=True)
 
-    for src_file in glob.glob(os.path.join(takeout_dir, '**/metadata.json'), recursive=True):
-        folder = os.path.dirname(src_file)
+    for json_file in glob.glob(os.path.join(takeout_dir, '**/metadata.json'), recursive=True):
+        album_dir_name = os.path.basename(os.path.dirname(json_file))
         
-        # Если '12-7-10' или '12.07.10' или '2012-07-10 -' или '2012-07-10 #2' или '2013-03-22-23',
-        # то не подходит
-        if re.match(r'^\d{1,4}(.|-)\d{1,2}(.|-)\d{2}$', os.path.basename(folder)) \
-            or re.match(r'^\d{1,4}-\d{1,2}-\d{2}\s?-$', os.path.basename(folder)) \
-            or re.match(r'^\d{1,4}-\d{1,2}-\d{2}-\d{2}$', os.path.basename(folder)) \
-            or re.match(r'^\d{1,4}-\d{1,2}-\d{2}\s{1}#\d{1}$', os.path.basename(folder)):
+        # If '12-7-10' or '12.07.10' or '2012-07-10 -' or '2012-07-10 #2' or '2013-03-22-23'
+        # then skip
+        if re.match(r'^\d{1,4}(.|-)\d{1,2}(.|-)\d{2}$', album_dir_name) \
+            or re.match(r'^\d{1,4}-\d{1,2}-\d{2}\s?-$', album_dir_name) \
+            or re.match(r'^\d{1,4}-\d{1,2}-\d{2}-\d{2}$', album_dir_name) \
+            or re.match(r'^\d{1,4}-\d{1,2}-\d{2}\s{1}#\d{1}$', album_dir_name):
             continue
 
-        print('Found album: {0}'.format(os.path.basename(folder)))
+        print('Found album directory {0}'.format(album_dir_name))
 
-        with open(src_file, encoding='utf-8') as metadata_file:
+        with open(json_file, encoding='utf-8') as metadata_file:
             metadata = json.load(metadata_file)['albumData']
 
             albumdata = {}
@@ -50,64 +97,33 @@ if __name__ == '__main__':
             albumdata['date'] = metadata['date']
             albumdata['files'] = []
 
-            for photo_file in glob.glob(os.path.join(folder, '*.jpg')):
-                with open(photo_file, 'rb') as file:
-                    try:
-                        tags = exifread.process_file(file, details=False)
-                        if 'EXIF DateTimeOriginal' not in tags.keys():
-                            print('[ERROR] Не могу поместить фото в альбом {0}: no tags in {1}'.format(albumdata['title'], photo_file))
-                            continue
+            for media_file in glob.glob(os.path.join(os.path.dirname(json_file), '*.*')):
+                if media_file.lower().endswith('.jpg'):
+                    albumdata['files'].append(process_photo(media_file))
+                if media_file.lower().endswith('.mp4') or media_file.lower().endswith('.mov'):
+                    if zipfile.is_zipfile(media_file):
+                        try:
+                            tmp_file = os.path.join(tmp_dir, os.path.basename(media_file))
 
-                        dt = datetime.datetime.strptime(str(tags['EXIF DateTimeOriginal']).split(' ')[0], '%Y:%m:%d')
+                            z = zipfile.ZipFile(media_file)
+                            name = z.namelist()[0]
 
-                        albumdata['files'].append('{0}/{1}'.format(dt.strftime('%Y-%m-%d'), os.path.basename(photo_file)))
+                            with z.open(name) as source:
+                                with open(tmp_file, "wb") as target:
+                                    shutil.copyfileobj(source, target)
 
-                    except PermissionError:
-                        print('[ERROR] Не могу поместить фото в альбом {0}: permission error for {1}'.format(albumdata['title'], photo_file))
+                            albumdata['files'].append(process_video(tmp_file))
 
-            for video_file in glob.glob(os.path.join(folder, '*.mp4')):
-                if zipfile.is_zipfile(video_file):
-                    z = zipfile.ZipFile(video_file)
+                        except zipfile.BadZipFile:
+                            print("[ERROR] BadZipFile {0}".format(media_file))
 
-                    try:
-                        name = z.namelist()[0]
-                    
-                        tmp_file = os.path.join(tmp_dir, 'extracted_{0}'.format(os.path.basename(name)))
-                        target = open(tmp_file, "wb")
-                        source = z.open(name)
-                        with source, target:
-                            shutil.copyfileobj(source, target)
-
-                        cmnd = ['ffprobe.exe', '-show_streams', '-print_format', 'json', '-loglevel', 'quiet', '{0}'.format(tmp_file)]
-                        p = subprocess.Popen(cmnd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-                        out, err = p.communicate()
-                        
-                        info = json.loads(out.decode('utf-8'))
-                        tags_found = False
-
-                        for stream in info['streams']:
-                            if not 'tags' in stream:
-                                continue
-
-                            tags = stream['tags']
-
-                            if 'creation_time' in tags:
-                                dt = datetime.datetime.strptime(tags['creation_time'], '%Y-%m-%dT%H:%M:%S.%fZ')
-                                albumdata['files'].append('{0}/{1}'.format(dt.strftime('%Y-%m-%d'), os.path.basename(video_file)))
-                                tags_found = True
-                                break
-
-                        if not tags_found:
-                            print('[ERROR] No tags found in {0}'.format(video_file))
-                        
-                        os.remove(tmp_file)
-                    
-                    except zipfile.BadZipFile:
-                        print("[ERROR] Bad zip file {0}".format(video_file))    
-                else:
-                    print("[ERROR] {0} is not zip archive".format(video_file))
+                        finally:
+                            safe_remove_file(tmp_file)
+                    else:
+                        albumdata['files'].append(process_video(media_file))
 
             with open(os.path.join(out_dir, str(uuid.uuid4()) + '.json'), 'w', encoding='utf-8') as outfile:
+                albumdata['files'] = list(filter(None, albumdata['files']))
                 json.dump(albumdata, outfile, ensure_ascii=False, indent=4)
 
     shutil.rmtree(tmp_dir)
