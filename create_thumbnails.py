009bedc3ba2465657a107561c4bf4905e9af7025
kondrikov <kondrikov@ascon.ru>
2019-01-23 16:34:50 +0300

    Create thumbnails for video files

--- create_thumbnails.py
+++ create_thumbnails.py
@@ -3,16 +3,23 @@
 import argparse
 import glob
 import os
+import subprocess
 
 from PIL import Image, ImageOps
 
 
+def create_thumbnail(input_file, out_file, size):
+    thumbnail = ImageOps.fit(Image.open(input_file), size, Image.ANTIALIAS)
+    os.makedirs(os.path.dirname(out_file), exist_ok=True)
+    thumbnail.save(out_file, "JPEG")
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description= \
-        '''Create image thumbnails''')
+        '''Create thumbnails for image and video files (*.jpg; *.jpeg; *.mp4; *.mov)''')
         
     parser.add_argument('--src-dir', type=str, required=True,
-                        help='directory containing images')
+                        help='directory containing media files')
     parser.add_argument('--thumbnails-dir', type=str, required=True,
                         help='destination directory for thumbnails')
     parser.add_argument('--size', type=str, default='300,300',
@@ -26,17 +33,26 @@ if __name__ == '__main__':
     parts = size.split(',')
     thumbnail_size = int(parts[0].strip()), int(parts[1].strip()) if parts and len(parts) == 2 else (300, 300)
 
-    for image_file in glob.glob(os.path.join(src_dir, '**/*.*'), recursive=True):
-        if not image_file.lower().endswith('.jpg') and not image_file.lower().endswith('.jpeg'):
+    for media_file in glob.glob(os.path.join(src_dir, '**/*.*'), recursive=True):
+        if not media_file.lower().endswith('.jpg') and \
+            not media_file.lower().endswith('.jpeg') and \
+            not media_file.lower().endswith('.mp4') and \
+            not media_file.lower().endswith('.mov'):
             continue
 
-        parentdir = os.path.basename(os.path.dirname(image_file))
-        basename = os.path.basename(image_file)
+        parent_dir = os.path.basename(os.path.dirname(media_file))
+        file_name = os.path.basename(media_file)
 
-        src_image = Image.open(image_file)
-        thumb_image = ImageOps.fit(src_image, thumbnail_size, Image.ANTIALIAS)
+        if file_name.lower().endswith('.mp4') or file_name.lower().endswith('.mov'):
+            tmp_file = os.sep.join([thumbnails_dir, '.video_thumbnail.jpg'])
+            cmnd = ['ffmpeg.exe', '-i', media_file, '-ss', '00:00:00', '-vframes', '1', tmp_file]
+            p = subprocess.Popen(cmnd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            _out, _err = p.communicate()
+
+            create_thumbnail(tmp_file, os.sep.join([thumbnails_dir, parent_dir,
+                '{0}.jpg'.format(os.path.splitext(file_name)[0])]), thumbnail_size)
+            os.remove(tmp_file)
+        else:
+            create_thumbnail(media_file, os.sep.join([thumbnails_dir, parent_dir,
+                '{0}.jpg'.format(os.path.splitext(file_name)[0])]), thumbnail_size)
         
-        save_dir = os.sep.join([thumbnails_dir, parentdir])
-        os.makedirs(save_dir, exist_ok=True)
-        thumb_image.save(os.sep.join([save_dir, basename]), "JPEG")
-
