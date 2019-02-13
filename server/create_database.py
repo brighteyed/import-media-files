e31eef45a974107933ad5f5ddf08a5417ae5aa87
kondrikov <kondrikov@ascon.ru>
2019-02-13 15:48:43 +0300

    Switch between Albums and Directories

--- server/create_database.py
+++ server/create_database.py
@@ -20,13 +20,15 @@ from models.mediadir import MediaDir
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser('''Create SQLite database containing info for all media files (*.jpg; *.jpeg; *.mp4; *.mov)''')
-    parser.add_argument('--src-dir', type=str,
+    parser.add_argument('--media-dir', type=str, required=True,
                         help='directory containing imported photos, videos and albums metadata files')
-    parser.add_argument('--db-file', type=str, 
+    parser.add_argument('--albums-dir', type=str, required=True,
+                        help='directory containing albums')
+    parser.add_argument('--db-file', type=str, required=True, 
                         help='path to output database file')
     args = parser.parse_args()
 
-    src_dir = args.src_dir
+    media_dir = args.media_dir
     db_file = args.db_file
 
     engine = create_engine("sqlite:///{0}".format(db_file))
@@ -37,74 +39,75 @@ if __name__ == "__main__":
     session = Session()
 
     try:
-        media_dirs = {}
-        for basename in os.listdir(src_dir):
-            dirpath = os.path.join(src_dir, basename)
-            if os.path.isdir(dirpath):
-                metadata_file = os.path.join(dirpath, '{0}.json'.format(basename))
-                display_name = basename
-                is_album = False
-                if os.path.isfile(metadata_file):
-                    is_album = True
-                    with open(metadata_file, encoding='utf-8') as albumdata:
-                        display_name = json.load(albumdata)['title']
+        for src_dir in [args.media_dir, args.albums_dir]:
+            media_dirs = {}
+            for basename in os.listdir(src_dir):
+                dirpath = os.path.join(src_dir, basename)
+                if os.path.isdir(dirpath):
+                    metadata_file = os.path.join(dirpath, '{0}.json'.format(basename))
+                    display_name = basename
+                    is_album = False
+                    if os.path.isfile(metadata_file):
+                        is_album = True
+                        with open(metadata_file, encoding='utf-8') as albumdata:
+                            display_name = json.load(albumdata)['title']
 
-                dir_entry = MediaDir(basename, display_name, is_album)
-                media_dirs[basename] = dir_entry
-                session.add(dir_entry)
+                    dir_entry = MediaDir(basename, display_name, is_album)
+                    media_dirs[basename] = dir_entry
+                    session.add(dir_entry)
 
-        session.flush()
+            session.flush()
 
-        for src_file in glob.glob(os.path.join(src_dir, '**/*.*'), recursive=True):
-            if src_file.lower().endswith('.jpg') or src_file.lower().endswith('.jpeg'):
+            for src_file in glob.glob(os.path.join(src_dir, '**/*.*'), recursive=True):
+                if src_file.lower().endswith('.jpg') or src_file.lower().endswith('.jpeg'):
 
-                with open(src_file, 'rb') as file:
-                    tags = exifread.process_file(file, details=False)
+                    with open(src_file, 'rb') as file:
+                        tags = exifread.process_file(file, details=False)
+                        
+                        tag = None
+                        if 'EXIF DateTimeOriginal' in tags.keys():
+                            tag = tags['EXIF DateTimeOriginal']
+                        elif 'EXIF DateTimeDigitized' in tags.keys():
+                            tag = tags['EXIF DateTimeDigitized']
+
+                        if not tag:
+                            print('[ERROR] Creation time not found in {0}'.format(src_file))
+                            continue
+
+                        try:
+                            session.add(MediaFile(media_dirs[os.path.basename(os.path.dirname(src_file))].id,
+                                                    os.path.basename(src_file),
+                                                    datetime.strptime(str(tag), '%Y:%m:%d %H:%M:%S')))
+
+                        except ValueError:
+                            print('[ERROR] Creation time not found in {0}'.format(src_file))
+                
+                elif src_file.lower().endswith('.mp4') or src_file.lower().endswith('.mov'):
                     
-                    tag = None
-                    if 'EXIF DateTimeOriginal' in tags.keys():
-                        tag = tags['EXIF DateTimeOriginal']
-                    elif 'EXIF DateTimeDigitized' in tags.keys():
-                        tag = tags['EXIF DateTimeDigitized']
+                    cmnd = ['ffprobe.exe', '-show_format', '-print_format', 'json', '-loglevel', 'quiet', '{0}'.format(src_file)]
+                    p = subprocess.Popen(cmnd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                    out, _err = p.communicate()
+                    
+                    info = json.loads(out.decode('utf-8'))
+                    creation_time_found = False
 
-                    if not tag:
+                    if 'format' in info and 'tags' in info['format']:
+                        tags = info['format']['tags']
+                        if 'creation_time' in tags:
+                            creation_time_found = True
+
+                            now_timestamp = time.time()
+                            utc_offset = datetime.fromtimestamp(now_timestamp) - datetime.utcfromtimestamp(now_timestamp)
+                            dt = datetime.strptime(tags['creation_time'], '%Y-%m-%dT%H:%M:%S.%fZ') + utc_offset
+
+                            session.add(MediaFile(media_dirs[os.path.basename(os.path.dirname(src_file))].id,
+                                                    os.path.basename(src_file),
+                                                    dt))
+
+                    if not creation_time_found:
                         print('[ERROR] Creation time not found in {0}'.format(src_file))
-                        continue
 
-                    try:
-                        session.add(MediaFile(media_dirs[os.path.basename(os.path.dirname(src_file))].id,
-                                                os.path.basename(src_file),
-                                                datetime.strptime(str(tag), '%Y:%m:%d %H:%M:%S')))
-
-                    except ValueError:
-                        print('[ERROR] Creation time not found in {0}'.format(src_file))
-            
-            elif src_file.lower().endswith('.mp4') or src_file.lower().endswith('.mov'):
-                
-                cmnd = ['ffprobe.exe', '-show_format', '-print_format', 'json', '-loglevel', 'quiet', '{0}'.format(src_file)]
-                p = subprocess.Popen(cmnd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-                out, _err = p.communicate()
-                
-                info = json.loads(out.decode('utf-8'))
-                creation_time_found = False
-
-                if 'format' in info and 'tags' in info['format']:
-                    tags = info['format']['tags']
-                    if 'creation_time' in tags:
-                        creation_time_found = True
-
-                        now_timestamp = time.time()
-                        utc_offset = datetime.fromtimestamp(now_timestamp) - datetime.utcfromtimestamp(now_timestamp)
-                        dt = datetime.strptime(tags['creation_time'], '%Y-%m-%dT%H:%M:%S.%fZ') + utc_offset
-
-                        session.add(MediaFile(media_dirs[os.path.basename(os.path.dirname(src_file))].id,
-                                                os.path.basename(src_file),
-                                                dt))
-
-                if not creation_time_found:
-                    print('[ERROR] Creation time not found in {0}'.format(src_file))
-
-        session.commit()
+            session.commit()
 
     except:
         print('[ERROR] Error while creating database')
--- server/media_server.py
+++ server/media_server.py
@@ -4,7 +4,7 @@ import argparse
 import json
 import os
 
-from flask import render_template, request, send_file, url_for
+from flask import session, render_template, request, send_file, url_for
 
 from models.mediadir import MediaDir
 from models.mediafile import MediaFile
@@ -16,14 +16,19 @@ from app import app, app_db
 def index():
     """ List all directories with pagination support """
 
-    media_dirs = app_db.session.query(MediaDir).order_by(MediaDir.display_name).paginate(request.args.get('page', 1, type=int), DIRS_PER_PAGE, False)
+    is_albums_mode = request.args.get('albums', False, type=bool)
+    session['is_albums_mode'] = is_albums_mode
+
+    media_dirs_query = app_db.session.query(MediaDir).filter_by(is_album=is_albums_mode).order_by(MediaDir.display_name)
+    media_dirs = media_dirs_query.paginate(request.args.get('page', 1, type=int), DIRS_PER_PAGE, False)
     next_url = url_for('index', page=media_dirs.next_num) if media_dirs.has_next else None
     prev_url = url_for('index', page=media_dirs.prev_num) if media_dirs.has_prev else None
 
     return render_template('index.html', 
                 folders=[(entry.folder_basename, entry.display_name) for entry in media_dirs.items],
                 next_url=next_url,
-                prev_url=prev_url)
+                prev_url=prev_url,
+                is_albums_mode=is_albums_mode)
 
 @app.route('/<dirname>')
 def viewdir(dirname):
@@ -46,7 +51,8 @@ def getfile(folder, file):
     if thumb == 1:
         return send_file(os.sep.join([THUMBNAILS_DIR, folder, '{0}.jpg'.format(os.path.splitext(file)[0])]), mimetype='image/jpg')
 
-    return send_file(os.sep.join([ROOT_DIR, folder, file]), mimetype='video/mp4' if file_utils.is_video_file(file) else 'image/jpg')
+    is_albums_mode = session.get('is_albums_mode', False)
+    return send_file(os.sep.join([ROOT_DIR if not is_albums_mode else ALBUMS_DIR, folder, file]), mimetype='video/mp4' if file_utils.is_video_file(file) else 'image/jpg')
 
 @app.route('/favicon.ico')
 def favicon():
@@ -58,17 +64,20 @@ if __name__ == '__main__':
         
     parser.add_argument('--thumbnails-dir', type=str, required=True, help='destination containing thumbnails')
     parser.add_argument('--media-dir', type=str, required=True, help='directory containing media files (*.jpg; *.jpeg; *.mp4; *.mov')
+    parser.add_argument('--albums-dir', type=str, required=True, help='directory containing albums')
     parser.add_argument('--db-file', type=str, required=True, help='path to media info database file')
     args = parser.parse_args()
 
     THUMBNAILS_DIR = args.thumbnails_dir
+    ALBUMS_DIR = args.albums_dir
     ROOT_DIR = args.media_dir
 
     FILES_PER_PAGE=8
-    DIRS_PER_PAGE=25
+    DIRS_PER_PAGE=40
 
     app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///{0}".format(args.db_file)
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY') or 'for-debug-only'
     app_db.init_app(app)
 
     app.run()
